99b071a61fe043df0c358d6b7993d62d67abedef
Nastia-pro <anastasiiapleshkova96@gmail.com>
2022-06-27 18:06:00 -0400

    heey look at the line

--- src/Practice.java
+++ src/Practice.java
@@ -1,6 +1,7 @@
 public class Practice {
     public static void main(String[] args) {
         System.out.println("Hey there");
-        System.out.println("new line");
+
+        System.out.println("hey there new line");
     }
 }
