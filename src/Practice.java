6d90125d42d63ed57d076ed3975220f19a6a4c4f
Nastia-pro <anastasiiapleshkova96@gmail.com>
2022-06-27 19:34:48 -0400

    hey, new line alert for group2

--- src/Practice.java
+++ src/Practice.java
@@ -1,7 +1,7 @@
 public class Practice {
     public static void main(String[] args) {
         System.out.println("Hey there");
-
         System.out.println("hey there new line");
+        System.out.println("Hello group2!");
     }
 }
