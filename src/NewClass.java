0759995541c253a8820f6a793f10929a49f5953f
“Nastia-pro git config --global user.email “your_github_email”anastasiiapleshkova96@gmail.com <anastasiiapleshkova96@gmail.com>
2022-07-01 13:16:17 -0400

    updated word class

--- src/NewClass.java
+++ src/NewClass.java
@@ -1,6 +1,6 @@
 public class NewClass {
     public static void main(String[] args) {
 
-        System.out.println("Hey from main clas");
+        System.out.println("Hey from main class");
     }
 }
